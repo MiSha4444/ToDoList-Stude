49a76eb9e0aa4f65a079223cb514e38b08a6df55
MSHabanov <misnash4895@gmail.com>
2023-11-27 17:22:49 +0300

    Добавил компонент удаления добавления категорий

---
+++ src/app/tasks-categories/adding-deleting-categories/adding-deleting-categories.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-adding-deleting-categories',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './adding-deleting-categories.component.html',
+  styleUrl: './adding-deleting-categories.component.scss'
+})
+export class AddingDeletingCategoriesComponent {
+
+}
