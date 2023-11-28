588ca0245bd8aa3ce90d84d19c011c24d3ad5d02
MSHabanov <misnash4895@gmail.com>
2023-11-28 17:03:45 +0300

    Добавлена динамическая смена страниц

---
+++ src/app/tasks-categories/create-editcategories/create-editcategories.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-create-editcategories',
+  templateUrl: './create-editcategories.component.html',
+  styleUrls: ['./create-editcategories.component.scss']
+})
+export class CreateEditcategoriesComponent {
+
+}
