b5ba43a7ee370e8aca84eeb8ac86bacb576dacc5
MSHabanov <misnash4895@gmail.com>
2023-11-28 12:48:06 +0300

    Подключил компноненты к модулю авторизации

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {TasksCategoriesModule} from "./tasks-categories/tasks-categories.module";
+import {TasksModule} from "./tasks/tasks.module";
+import {AuthenficationModule} from "./authenfication/authenfication.module";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TasksCategoriesModule,
+    TasksModule,
+    AuthenficationModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/authenfication/authorization/authorization.component.ts
+++ src/app/authenfication/authorization/authorization.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-authorization',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './authorization.component.html',
-  styleUrl: './authorization.component.scss'
+  styleUrls: ['./authorization.component.scss']
 })
 export class AuthorizationComponent {
 
