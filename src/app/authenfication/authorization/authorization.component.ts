360a0345ad47122406b291b0d90a8bba41f7dfc8
MSHabanov <misnash4895@gmail.com>
2023-11-29 13:32:26 +0300

    feat: основные параметры компонента авторизации

--- src/app/authenfication/authorization/authorization.component.ts
+++ src/app/authenfication/authorization/authorization.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, } from '@angular/core';
 
 @Component({
   selector: 'app-authorization',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./authorization.component.scss']
 })
 export class AuthorizationComponent {
-
+  public login: string ='';
+  public password: string = '';
+  constructor() {
+    console.log(this.login,this.password)
+  }
+  authorizationEvent(){
+    console.log('dada');
+    if (this.login == 'misha' && this.password == '4895')
+    {
+      console.log('вход');
+    }
+  }
 }
