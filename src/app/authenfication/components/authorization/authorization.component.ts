5b23d641debd64f7bd43f23f9c87a018200a6448
MSHabanov <misnash4895@gmail.com>
2023-11-30 16:08:44 +0300

    feat: добавлены валидаторы для Электронной почты и пароля

--- src/app/authenfication/components/authorization/authorization.component.ts
+++ src/app/authenfication/components/authorization/authorization.component.ts
@@ -1,19 +1,23 @@
-import {Component, } from '@angular/core';
+import {Component, OnInit,} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-authorization',
   templateUrl: './authorization.component.html',
   styleUrls: ['./authorization.component.scss']
 })
-export class AuthorizationComponent {
-  public login: string ='';
-  public password: string = '';
-
-  authorizationEvent(){
-    console.log('dada');
-    if (this.login == 'misha' && this.password == '4895')
-    {
-      console.log('вход');
-    }
+export class AuthorizationComponent implements OnInit{
+  authorizationform!: FormGroup;
+  ngOnInit() {
+    this.authorizationform = new FormGroup({
+      email: new FormControl('',[
+        Validators.email,
+        Validators.required,
+      ]),
+      password: new FormControl('',[Validators.required])
+    })
+  }
+  submit(){
+    console.log(this.authorizationform)
   }
 }
