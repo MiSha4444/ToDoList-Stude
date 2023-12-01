6110c344e7d14cdec203a2b5394b097b9362083d
MSHabanov <misnash4895@gmail.com>
2023-12-01 16:55:01 +0300

    feat: сделан прототип авторизации

--- src/app/authenfication/components/authorization/authorization.component.ts
+++ src/app/authenfication/components/authorization/authorization.component.ts
@@ -1,24 +1,53 @@
 import {Component, OnInit,} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {VerificationService} from "../../service/verification.service";
+
+interface user {
+  username: string
+  password: string
+}
 
 @Component({
   selector: 'app-authorization',
   templateUrl: './authorization.component.html',
-  styleUrls: ['./authorization.component.scss']
+  styleUrls: ['./authorization.component.scss'],
+  providers: [VerificationService],
 })
-export class AuthorizationComponent implements OnInit{
+export class AuthorizationComponent implements OnInit {
+  authorizationFlag: boolean = false;
+
   authorizationform!: FormGroup;
+
+  users: user[] = [{
+    username: "misnash4895@gmail.com",
+    password: "1111"
+  }, {
+    username: "test2@g",
+    password: "2222"
+  }];
+
   ngOnInit() {
     this.authorizationform = new FormGroup({
-      email: new FormControl('',[
+      login: new FormControl('', [
         Validators.email,
         Validators.required,
       ]),
-      password: new FormControl('',[Validators.required])
+      password: new FormControl('', [Validators.required])
 
     })
   }
-  submit(){
-    console.log(this.authorizationform)
+
+   public submit() {
+    if (this.authorizationform.valid && this.authorizationform.valid) {
+    }
+    const checkUser = this.users
+      .find(users => users.username == this.authorizationform.value.login) ?? '';
+    //console.log(checkUser.password)
+    if (checkUser) {
+      const i = checkUser.password == this.authorizationform.value.password ?
+        this.authorizationFlag = true
+        : this.authorizationFlag = false;
+    }
+
   }
 }
