ad8a35835d2239276ab07ddc72154f966a539a78
MSHabanov <misnash4895@gmail.com>
2023-12-01 15:27:55 +0300

    debug:

--- src/app/authenfication/components/registration/registration.component.ts
+++ src/app/authenfication/components/registration/registration.component.ts
@@ -1,26 +1,30 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {VerificationService} from "../../service/verification.service";
 
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
+  providers: [VerificationService]
 })
-export class RegistrationComponent implements OnInit{
+export class RegistrationComponent implements OnInit {
   registrationform!: FormGroup;
+
   ngOnInit() {
     this.registrationform = new FormGroup({
-      email: new FormControl('',[
+      email: new FormControl('', [
         Validators.email,
         Validators.required,
       ]),
-      password: new FormControl('dasd',[Validators.required,Validators.min(6)]),
-      date: new FormControl('',[Validators.required]),
+      password: new FormControl('dasd', [Validators.required, Validators.min(6)]),
+      date: new FormControl('', [Validators.required]),
       username: new FormControl('')
     })
   }
 
-  submit(){
+  submit() {
+
   }
 }
