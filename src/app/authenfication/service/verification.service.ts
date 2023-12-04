d3608cb75c4c4627826cd247c638ef0b106985d9
MSHabanov <misnash4895@gmail.com>
2023-12-04 09:24:22 +0300

    feat: сделан сервис верификации

--- src/app/authenfication/service/verification.service.ts
+++ src/app/authenfication/service/verification.service.ts
@@ -1,9 +1,48 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {FormGroup} from "@angular/forms";
+
+interface user {
+    username: string
+    password: string
+    date?: string
+    city?: string
+}
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
+
 export class VerificationService {
 
-  constructor() { }
+    public authorizationFlag: boolean = false;
+
+    private usersData: user[] = [{
+        username: "misnash4895@gmail.com",
+        password: "1111"
+    }, {
+        username: "test2@g",
+        password: "2222"
+    }];
+
+    constructor() {
+    }
+
+    public checkAuthorization(authorizationForm: FormGroup) {
+        if (authorizationForm.valid && authorizationForm.valid) {
+            const checkUser = this.usersData
+                .find(users => users.username == authorizationForm.value.login) ?? '';
+            //console.log(checkUser.password)
+            if (checkUser) {
+                const i = checkUser.password == authorizationForm.value.password ?
+                    this.authorizationFlag = true : this.authorizationFlag = false;
+            }
+        }
+    }
+
+    public userRegistration(registrationForm: FormGroup) {
+        this.usersData.push(registrationForm.value)
+        console.log(registrationForm.value)
+        console.log(this.usersData)
+    }
+
 }
