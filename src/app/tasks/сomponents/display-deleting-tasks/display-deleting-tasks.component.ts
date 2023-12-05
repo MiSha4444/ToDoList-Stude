75a48fe90d5acb6f8c52204b1ba6c9764e47a9ca
MSHabanov <misnash4895@gmail.com>
2023-12-05 13:10:15 +0300

    feat: добавлены тестовые данные

--- src/app/tasks/сomponents/display-deleting-tasks/display-deleting-tasks.component.ts
+++ src/app/tasks/сomponents/display-deleting-tasks/display-deleting-tasks.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-display-deleting-tasks',
@@ -7,5 +7,24 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./display-deleting-tasks.component.scss']
 })
 export class DisplayDeletingTasksComponent {
+  cols:any[] = [
+    {field: 'user', header: 'Исполнитель'},
+    {field: 'name', header: 'название задачи'},
+    {field: 'status', header: 'Статус'},
+    {field: 'date',header: 'Дата'}
+  ]
 
+  tasks: any[]= [
+    {status:'выполнено',name:'Помыть пол',user:'Миша',date:'15.11.2023'},
+    {status:'прросрочено',name:'Выключить утюг',user:'Тест1',date:'29.11.2023'},
+    {status:'в работе',name:'Протереть пыли',user:'Маша',date:'30.11.2023'},
+    {status:'выполнено',name:'Покормить рыбок',user:'Папа',date:'3.12.2023'},
+    {status:'прросрочено',name:'Оплатить квитанции',user:'Мама',date:'23.11.2023'},
+    {status:'выполнено',name:'пропылесосить',user:'Робот пылесос',date:'4.12.2023'},
+    {status:'прросрочено',name:'Помыть посуду',user:'Тест2',date:'29.11.2023'},
+    {status:'в работе',name:'купить продуктов',user:'Папа',date:'2.12.2023'},
+    {status:'прросрочено',name:'Купить молоко',user:'Мама',date:'2.12.2023'},
+    {status:'в работе',name:'Снять квартиру на новый год',user:'Маша',date:'5.11.2023'},
+    {status:'выполнено',name:'Купить подарки на нг',user:'Мища',date:'5.12.2023'},
+  ]
 }
