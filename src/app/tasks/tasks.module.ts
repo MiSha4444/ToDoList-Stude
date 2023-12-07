578003717f3922e217f6e80adc10628da7f25e8f
MSHabanov <misnash4895@gmail.com>
2023-12-07 17:45:53 +0300

    feat: добавлены модули отображения

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -4,7 +4,7 @@ import {EditingCreatingTasksComponent} from "./сomponents/editing-creating-task
 import {DisplayDeletingTasksComponent} from "./сomponents/display-deleting-tasks/display-deleting-tasks.component";
 import {RouterModule} from "@angular/router";
 import {TableModule} from "primeng/table";
-import {NgForOf, NgIf} from "@angular/common";
+import {JsonPipe, NgForOf, NgIf} from "@angular/common";
 import {ToolbarModule} from "primeng/toolbar";
 import {DialogModule} from "primeng/dialog";
 import {RadioButtonModule} from "primeng/radiobutton";
@@ -12,6 +12,10 @@ import {FormsModule} from "@angular/forms";
 import {RippleModule} from "primeng/ripple";
 import {ButtonModule} from "primeng/button";
 import {InputTextareaModule} from "primeng/inputtextarea";
+import {ChipsModule} from "primeng/chips";
+import {DataViewModule} from "primeng/dataview";
+import {DropdownModule} from "primeng/dropdown";
+import {CalendarModule} from "primeng/calendar";
 
 
 
@@ -21,18 +25,23 @@ import {InputTextareaModule} from "primeng/inputtextarea";
     EditingCreatingTasksComponent,
     DisplayDeletingTasksComponent,
   ],
-  imports: [
-    TableModule,
-    NgForOf,
-    ToolbarModule,
-    DialogModule,
-    RadioButtonModule,
-    FormsModule,
-    NgIf,
-    RippleModule,
-    ButtonModule,
-    InputTextareaModule
-  ],
-  exports:[RouterModule]
+    imports: [
+        TableModule,
+        NgForOf,
+        ToolbarModule,
+        DialogModule,
+        RadioButtonModule,
+        FormsModule,
+        NgIf,
+        RippleModule,
+        ButtonModule,
+        InputTextareaModule,
+        ChipsModule,
+        DataViewModule,
+        DropdownModule,
+        JsonPipe,
+        CalendarModule
+    ],
+  exports:[RouterModule],
 })
 export class TasksModule { }
