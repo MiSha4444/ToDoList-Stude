ffb7aec87907c8e427fc5786049778b09b64f88c
MSHabanov <misnash4895@gmail.com>
2023-12-07 17:45:28 +0300

    feat: добавлены сервисы для подтверждения удаления

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {TasksCategoriesModule} from "./tasks-categories/tasks-categories.module";
 import {TasksModule} from "./tasks/tasks.module";
 import {AuthenficationModule} from "./authenfication/authenfication.module";
 import {RouterModule} from "@angular/router";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-
+import {ConfirmationService, MessageService} from "primeng/api";
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    RouterModule,
-    BrowserModule,
-    AppRoutingModule,
-    TasksCategoriesModule,
-    TasksModule,
-    AuthenficationModule,
-    FormsModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        RouterModule,
+        BrowserModule,
+        AppRoutingModule,
+        TasksCategoriesModule,
+        TasksModule,
+        AuthenficationModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+    ],
+    providers: [MessageService, ConfirmationService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
