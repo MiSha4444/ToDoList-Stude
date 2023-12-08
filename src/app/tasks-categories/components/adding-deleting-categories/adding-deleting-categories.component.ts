7340dab64fffc9fd2c6b89dafcfda88d29a6831e
MSHabanov <misnash4895@gmail.com>
2023-12-08 21:03:48 +0300

    feat: добавленны методы для кнопок  удаления и редактирования категорий

--- src/app/tasks-categories/components/adding-deleting-categories/adding-deleting-categories.component.ts
+++ src/app/tasks-categories/components/adding-deleting-categories/adding-deleting-categories.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {ConfirmationService, MessageService} from "primeng/api";
+
 
 @Component({
   selector: 'app-adding-deleting-categories',
@@ -8,5 +10,25 @@ import {CommonModule} from '@angular/common';
 })
 export class AddingDeletingCategoriesComponent {
   public categories: string[] = ['Домашние дела', 'Учеба', 'Работа']
+  category: any
 
+  constructor(public messageService: MessageService, public confirmationService: ConfirmationService) {
+  }
+
+  editCategory() {
+
+  }
+
+  deleteCategory(category: any) {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete ' + this.category + '?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.category = {};
+        this.messageService.add({severity: 'success', summary: 'Successful', detail: 'Task Deleted', life: 3000});
+      }
+
+    });
+  }
 }
