03ce3494f95b4e10f8808f49bf6a121d3d59efb9
MSHabanov <misnash4895@gmail.com>
2023-12-08 21:05:24 +0300

    feat: добавленны модули для работы с шаблоном

--- src/app/tasks-categories/tasks-categories.module.ts
+++ src/app/tasks-categories/tasks-categories.module.ts
@@ -3,15 +3,26 @@ import { NgModule } from '@angular/core';
 import {AddingDeletingCategoriesComponent} from "./components/adding-deleting-categories/adding-deleting-categories.component";
 import {CreateEditcategoriesComponent} from "./components/create-editcategories/create-editcategories.component";
 import {RouterModule} from '@angular/router';
+import {FormsModule} from "@angular/forms";
+import {NgForOf} from "@angular/common";
+import {CardModule} from "primeng/card";
+import {ButtonModule} from "primeng/button";
+import {RippleModule} from "primeng/ripple";
 
 
 
 @NgModule({
   declarations:[
     CreateEditcategoriesComponent,
-    AddingDeletingCategoriesComponent
+    AddingDeletingCategoriesComponent,
+
   ],
   imports: [
+    FormsModule,
+    NgForOf,
+    CardModule,
+    ButtonModule,
+    RippleModule
   ],
   exports:[RouterModule]
 })
