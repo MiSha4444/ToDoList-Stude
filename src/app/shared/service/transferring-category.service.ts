ef61172f63ea1e7c46a8ebd594c2f2d7a75580d8
MSHabanov <misnash4895@gmail.com>
2023-12-11 18:31:49 +0300

    refactor

---
+++ src/app/shared/service/transferring-category.service.ts
@@ -0,0 +1,10 @@
+import {Injectable, Input, Output} from '@angular/core';
+import {category} from "../../tasks-categories/components/adding-deleting-categories/adding-deleting-categories.component";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TransferringCategoryService {
+
+}
