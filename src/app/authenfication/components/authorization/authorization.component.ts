d3a26d6b654f317ad7b739e4a238df516016474c
MSHabanov <misnash4895@gmail.com>
2023-12-12 22:32:00 +0300

    Test: RXJS

--- src/app/authenfication/components/authorization/authorization.component.ts
+++ src/app/authenfication/components/authorization/authorization.component.ts
@@ -1,40 +1,49 @@
 import {Component, OnInit,} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {VerificationService} from "../../service/verification.service";
+import {BehaviorSubject, interval, Observable, ReplaySubject, Subject, take} from "rxjs";
 
 interface user {
-  username: string
-  password: string
+    username: string
+    password: string
 }
 
 @Component({
-  selector: 'app-authorization',
-  templateUrl: './authorization.component.html',
-  styleUrls: ['./authorization.component.scss'],
-  providers: [VerificationService],
+    selector: 'app-authorization',
+    templateUrl: './authorization.component.html',
+    styleUrls: ['./authorization.component.scss'],
+    providers: [VerificationService],
 })
 
 export class AuthorizationComponent implements OnInit {
-  public authorizationFlag: boolean = this.verificationService.authorizationFlag;
+    public authorizationFlag: boolean = this.verificationService.authorizationFlag;
 
-  public authorizationForm!: FormGroup;
+    public authorizationForm!: FormGroup;
 
-  constructor(private verificationService: VerificationService) {
+    constructor(private verificationService: VerificationService) {
 
-  }
+        const obs$ = new Subject();
+        obs$.next(1)
+        obs$.next(2)
+        obs$.next(3)
+        obs$.next(4)
+        obs$.next(5)
+        obs$.subscribe(val => console.log('Подписчик1',val))
+        obs$.subscribe(val => console.log('Подписчик2',val))
+    }
 
-  ngOnInit() {
-    this.authorizationForm = new FormGroup({
-      login: new FormControl('', [
-        Validators.email,
-        Validators.required,
-      ]),
-      password: new FormControl('', [Validators.required])
+    ngOnInit() {
+        this.authorizationForm = new FormGroup({
+            login: new FormControl('', [
+                Validators.email,
+                Validators.required,
+            ]),
+            password: new FormControl('', [Validators.required])
 
-    })
-  }
+        })
+    }
 
-   public submit() {
-      this.verificationService.checkAuthorization(this.authorizationForm);
+    public submit() {
+        this.verificationService.checkAuthorization(this.authorizationForm);
     }
 }
