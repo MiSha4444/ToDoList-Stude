e0102d423293f5d53a4f4a8c3ea923f8bca996c0
MSHabanov <misnash4895@gmail.com>
2023-12-15 20:19:28 +0300

    feat: добавлена страница выхода из профиля

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {AuthorizationComponent} from "./authenfication/components/authorization/authorization.component";
 import {RegistrationComponent} from "./authenfication/components/registration/registration.component";
-import {DisplayDeletingTasksComponent} from "./tasks/сomponents/display-deleting-tasks/display-deleting-tasks.component";
 import {
-  AddingDeletingCategoriesComponent
+    DisplayDeletingTasksComponent
+} from "./tasks/сomponents/display-deleting-tasks/display-deleting-tasks.component";
+import {
+    AddingDeletingCategoriesComponent
 } from "./tasks-categories/components/adding-deleting-categories/adding-deleting-categories.component";
+import {gurdsGuard} from "./gurds.guard";
+import {ExitComponent} from "./authenfication/components/exit/exit.component";
 
-const routes:Routes =[
-  {path: 'Authorization',component:AuthorizationComponent},
-  {path:'Registration',component:RegistrationComponent},
-  {path:'Tasks',component:DisplayDeletingTasksComponent},
-  {path:'TasksCategories',component:AddingDeletingCategoriesComponent},
+const routes: Routes = [
+    {path: 'Authorization', component: AuthorizationComponent},
+    {path: 'Registration', component: RegistrationComponent},
+    {path: 'Tasks', component: DisplayDeletingTasksComponent, canActivate: [gurdsGuard]},
+    {path: 'TasksCategories', component: AddingDeletingCategoriesComponent, canActivate: [gurdsGuard]},
+    {path: 'Exit',component:ExitComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
