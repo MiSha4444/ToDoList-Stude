6096b01f963a1048e9defe90256b061c21512e10
MSHabanov <misnash4895@gmail.com>
2023-12-15 20:19:37 +0300

    feat: добавлена страница выхода из профиля

--- src/app/authenfication/authenfication.module.ts
+++ src/app/authenfication/authenfication.module.ts
@@ -1,22 +1,28 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
 import {RegistrationComponent} from "./components/registration/registration.component";
 import {AuthorizationComponent} from "./components/authorization/authorization.component";
 import {RouterModule} from "@angular/router";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-
+import {ChipsModule} from "primeng/chips";
+import {ButtonModule} from "primeng/button";
+import {ExitComponent} from "./components/exit/exit.component";
 
 
 @NgModule({
   declarations: [
     RegistrationComponent,
     AuthorizationComponent,
+    ExitComponent
   ],
   imports: [
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ChipsModule,
+    ButtonModule
   ],
-  exports:[RouterModule]
+  exports: [RouterModule]
 
 })
-export class AuthenficationModule { }
+export class AuthenficationModule {
+}
