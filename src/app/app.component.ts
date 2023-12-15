0d925bb0d6342240999aa72b2eb5374af9b21747
MSHabanov <misnash4895@gmail.com>
2023-12-15 14:21:42 +0300

    feat: добавлен menubar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ToDoList-stude';
+    title = 'ToDoList-stude';
+    items: any
+
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Вход',
+                icon: 'pi pi-sign-in',
+                items: [
+                    {label: 'Авторизация', icon: 'pi pi-user', url: 'Authorization'},
+                    {label: 'Регистриция', icon: 'pi pi-user-plus', url: 'Registration',queryParams: 'transitionTo'},
+                    {label: 'Выход', icon: 'pi pi-user-minus', },
+                ]
+            },
+            {
+                label: 'Задачи',
+                icon: 'pi pi-fw pi-pencil',
+                items: [
+                    {label: 'Задачи', icon: 'pi  pi-table', url: '/TasksCategories'},
+                    {label: 'Категории', icon: 'pi  pi-list', url: '/Tasks'},
+                ]
+            }
+        ];
+    }
 }
