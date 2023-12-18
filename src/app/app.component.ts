ade235af0fe92384f650286e9bed26ed7dd00d86
MSHabanov <misnash4895@gmail.com>
2023-12-18 15:14:30 +0300

    feat: добавлен menubar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,33 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'ToDoList-stude';
-    items: any
+  title = 'ToDoList-stude';
+  items: any
 
-    ngOnInit() {
-        this.items = [
-            {
-                label: 'Вход',
-                icon: 'pi pi-sign-in',
-                items: [
-                    {label: 'Авторизация', icon: 'pi pi-user', routerLink: 'Authorization'},
-                    {label: 'Регистриция', icon: 'pi pi-user-plus', routerLink: 'Registration'},
-                    {label: 'Выход', icon: 'pi pi-user-minus', routerLink: 'Exit'},
-                ]
-            },
-            {
-                label: 'Задачи',
-                icon: 'pi pi-fw pi-pencil',
-                items: [
-                    {label: 'Задачи', icon: 'pi  pi-table', routerLink: 'TasksCategories'},
-                    {label: 'Категории', icon: 'pi  pi-list', routerLink: 'Tasks'},
-                ]
-            }
-        ];
-    }
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Вход',
+        icon: 'pi pi-sign-in',
+        items: [
+          {label: 'Авторизация', icon: 'pi pi-user', routerLink: 'Authorization'},
+          {label: 'Регистриция', icon: 'pi pi-user-plus', routerLink: 'Registration'},
+          {label: 'Выход', icon: 'pi pi-user-minus', routerLink: 'Exit'},
+        ]
+      },
+      {
+        label: 'Задачи',
+        icon: 'pi pi-fw pi-pencil',
+        items: [
+          {label: 'Задачи', icon: 'pi  pi-table', routerLink: 'Tasks'},
+          {label: 'Категории', icon: 'pi  pi-list', routerLink: 'TasksCategories'},
+        ]
+      },
+    ];
+  }
 }
