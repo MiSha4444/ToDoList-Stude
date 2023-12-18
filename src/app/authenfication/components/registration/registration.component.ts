e150f72efbd9b682af992daddaf1c705c7a58760
MSHabanov <misnash4895@gmail.com>
2023-12-18 15:18:35 +0300

    feat: добавлен переход на страницу авторизаци после успешной регистрации

--- src/app/authenfication/components/registration/registration.component.ts
+++ src/app/authenfication/components/registration/registration.component.ts
@@ -1,37 +1,38 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {VerificationService} from "../../service/verification.service";
-
+import {Router} from "@angular/router";
 
 @Component({
-    selector: 'app-registration',
-    templateUrl: './registration.component.html',
-    styleUrls: ['./registration.component.scss'],
-    providers: [VerificationService]
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.scss'],
+  providers: [VerificationService]
 })
 export class RegistrationComponent implements OnInit {
-    public registrationform!: FormGroup;
+  public registrationform!: FormGroup;
 
-    constructor(private verificationService: VerificationService) {
-    }
+  constructor(private verificationService: VerificationService, private router: Router) {
+  }
 
-    ngOnInit() {
-        this.registrationform = new FormGroup({
-            email: new FormControl('', [
-                Validators.email,
-                Validators.required,
-            ]),
-            password: new FormControl('', [Validators.required, Validators.min(6)]),
-            username: new FormControl('')
-        })
-    }
+  ngOnInit() {
+    this.registrationform = new FormGroup({
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required,
+      ]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      username: new FormControl('')
+    })
+  }
 
-    submitRegistration() {
-        let user = this.registrationform.value
-        // @ts-ignore
-        let newUserData = {...user, tasks: [], categories: []}
-        console.log(this.registrationform.value)
-        localStorage.setItem(this.registrationform.value.email, JSON.stringify(newUserData))
-    }
+  submitRegistration() {
+    let user = this.registrationform.value
+    // @ts-ignore
+    let newUserData = {...user, tasks: [], categories: []}
+    console.log(this.registrationform.value)
+    localStorage.setItem(this.registrationform.value.email, JSON.stringify(newUserData))
+    this.router.navigate(['Authorization'])
+  }
 
 }
