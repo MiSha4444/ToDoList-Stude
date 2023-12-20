d583b3722a1e66175677c0e001bc015bed74b26c
MSHabanov <misnash4895@gmail.com>
2023-12-20 18:07:21 +0300

    feat: Добавлено удаление категорий

--- src/app/tasks-categories/components/adding-deleting-categories/adding-deleting-categories.component.ts
+++ src/app/tasks-categories/components/adding-deleting-categories/adding-deleting-categories.component.ts
@@ -1,113 +1,115 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit,} from '@angular/core';
 import {ConfirmationService, MessageService} from "primeng/api";
 import {TransferringCategoryService} from "../../../shared/service/transferring-category.service";
+import {BehaviorSubject} from "rxjs";
 
 export interface category {
-    name: string
-    id: string
-    description?: string
+  name: string
+  id: string
+  description?: string
 }
 
 @Component({
-    selector: 'app-adding-deleting-categories',
-    templateUrl: './adding-deleting-categories.component.html',
-    styleUrls: ['./adding-deleting-categories.component.scss'],
-    providers: [TransferringCategoryService],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-adding-deleting-categories',
+  templateUrl: './adding-deleting-categories.component.html',
+  styleUrls: ['./adding-deleting-categories.component.scss'],
+  providers: [TransferringCategoryService,MessageService, ConfirmationService,],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AddingDeletingCategoriesComponent implements OnInit{
+export class AddingDeletingCategoriesComponent implements OnInit {
 
-    submitted: boolean = false;
+  public submitted: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false)
 
-    categoryDialog: boolean = false;
+  public categoryDialog: boolean = false;
 
-    category: any;
+  public category: any;
 
-    public categories:any
+  public categories: any;
 
-    cols: any[] = [
-        {field: 'name', header: 'Название категории',},
-        {field: 'description', header: 'Описание категории',},
-    ]
+  cols: any[] = [
+    {field: 'name', header: 'Название категории',},
+    {field: 'description', header: 'Описание категории',},
+  ]
 
-    constructor(public messageService: MessageService, public confirmationService: ConfirmationService, private transServis: TransferringCategoryService) {
+  constructor(public messageService: MessageService, public confirmationService: ConfirmationService, private transServise: TransferringCategoryService) {
+  }
+
+  ngOnInit() {
+    // @ts-ignore
+
+    this.categories = JSON.parse(localStorage.getItem(localStorage.getItem('авторизован'))).categories
+  }
+
+  hideDialog() {
+    this.categoryDialog = false;
+    this.submitted.next(false);
+  }
+
+  OpenNew() {
+    this.category = {};
+    this.submitted.next(false);
+    this.categoryDialog = true;
+  }
+
+  saveCategory() {
+    this.submitted.next(true);
+    if (this.category.name.trim()) {
+      if (this.category.id) {
+        this.categories[this.findIndexById(this.category.id)] = this.category;
+      } else {
+        this.category.id = this.createId();
+        this.categories.push(this.category);
+      }
+      this.transServise.getUserCategory(this.categories)
+      this.categories = [...this.categories];
+      this.categoryDialog = false;
+    }
+    this.categories = [...this.category];
+    this.categoryDialog = false;
+    this.category = {};
+  }
+
+  editCategory(category: category) {
+    this.category = {...category};
+    console.log(this.category = {...category})
+    this.categoryDialog = true;
+  }
+
+  deleteCategory(category: category) {
+    console.log('dadss')
+    this.confirmationService.confirm({
+      message: 'Вы правда хотите удалить категорию ' + this.category.name + '?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.categories = this.categories.filter((val: { id: string; }) => val.id !== category.id);
+        this.transServise.getUserCategory(this.categories);
+        this.messageService.add({severity: 'success', summary: 'Successful', detail: 'Product Category', life: 3000});
+      }
+
+    });
+    console.log('dad222')
+  }
+
+  findIndexById(id: string): number {
+    let index = -1;
+    for (let i = 0; i < this.categories.length; i++) {
+      if (this.categories[i].id === id) {
+        index = i;
+        break;
+      }
     }
 
-    ngOnInit() {
-        // @ts-ignore
-        this.categories = JSON.parse(localStorage.getItem(localStorage.getItem('авторизован'))).categories
+    return index;
+  }
+
+  createId(): string {
+    let id = '';
+    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < 5; i++) {
+      id += chars.charAt(Math.floor(Math.random() * chars.length));
     }
-
-    hideDialog() {
-        this.categoryDialog = false;
-        this.submitted = false;
-    }
-
-    OpenNew() {
-        this.category = {};
-        this.submitted = false;
-        this.categoryDialog = true;
-    }
-
-    saveCategory() {
-        this.submitted = true;
-        if (this.category.name.trim()) {
-            if (this.category.id) {
-                this.categories[this.findIndexById(this.category.id)] = this.category;
-            } else {
-                this.category.id = this.createId();
-                this.categories.push(this.category);
-            }
-            this.transServis.getUserCategory(this.categories)
-            this.categories = [...this.categories];
-            this.categoryDialog = false;
-        }
-        this.categories = [...this.category];
-        this.categoryDialog = false;
-        this.category = {};
-    }
-
-    editCategory(category: category) {
-        this.category = {...category};
-        console.log(this.category = {...category})
-        this.categoryDialog = true;
-    }
-
-    deleteCategory(category: category) {
-        console.log('dadss')
-        this.confirmationService.confirm({
-            message: 'Are you sure you want to delete ' + this.category.name + '?',
-            header: 'Confirm',
-            icon: 'pi pi-exclamation-triangle',
-            accept: () => {
-                this.categories = this.categories.filter((val: { id: string; }) => val.id !== category.id);
-                this.category = {};
-                this.messageService.add({severity: 'success', summary: 'Successful', detail: 'Product Category', life: 3000});
-            }
-
-        });
-        console.log('dad222')
-    }
-
-    findIndexById(id: string): number {
-        let index = -1;
-        for (let i = 0; i < this.categories.length; i++) {
-            if (this.categories[i].id === id) {
-                index = i;
-                break;
-            }
-        }
-
-        return index;
-    }
-
-    createId(): string {
-        let id = '';
-        var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        for (var i = 0; i < 5; i++) {
-            id += chars.charAt(Math.floor(Math.random() * chars.length));
-        }
-        console.log(id)
-        return id;
-    }
+    console.log(id)
+    return id;
+  }
 }
