a2b9945146b36817fd30dac679ed56c78a60de1a
MSHabanov <misnash4895@gmail.com>
2023-12-26 17:26:54 +0300

    feat: добавлен метод удаления задач

--- src/app/shared/service/transferring-category.service.ts
+++ src/app/shared/service/transferring-category.service.ts
@@ -2,25 +2,32 @@ import {Injectable, Input, Output} from '@angular/core';
 import {category, task} from "../interfaces";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class TransferringCategoryService {
 
-    GetUserTask(tasks: task[]) {
-        let login = localStorage.getItem('авторизован')
-        // @ts-ignore
-        let userData = JSON.parse(localStorage.getItem(login))
-        userData.tasks = tasks
-        // @ts-ignore
-        localStorage.setItem(login, JSON.stringify(userData))
-    }
+  GetUserTask(tasks: task[]) {
+    let login = localStorage.getItem('авторизован')
+    // @ts-ignore
+    let userData = JSON.parse(localStorage.getItem(login))
+    userData.tasks = tasks
+    // @ts-ignore
+    localStorage.setItem(login, JSON.stringify(userData))
+  }
 
-    getUserCategory(categories: category[]) {
-        let login = localStorage.getItem('авторизован')
-        // @ts-ignore
-        let userData = JSON.parse(localStorage.getItem(login))
-        userData.categories = categories
-        // @ts-ignore
-        localStorage.setItem(login, JSON.stringify(userData))
-    }
+  getUserCategory(categories: category[]) {
+    let login = localStorage.getItem('авторизован')
+    // @ts-ignore
+    let userData = JSON.parse(localStorage.getItem(login))
+    userData.categories = categories
+    // @ts-ignore
+    localStorage.setItem(login, JSON.stringify(userData))
+  }
+
+  deleteTask(task: task) {
+    let login = localStorage.getItem('авторизован')
+    // @ts-ignore
+    let userData = JSON.parse(localStorage.getItem(login))
+    userData.tasks
+  }
 }
