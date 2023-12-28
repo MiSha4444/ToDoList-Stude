104b0cb66a3a3072b955927f6678e0f40f098355
MSHabanov <misnash4895@gmail.com>
2023-12-28 18:25:22 +0300

    feat: Исправлен баг с отображением задач и категорий

--- src/app/shared/service/transferring-category.service.ts
+++ src/app/shared/service/transferring-category.service.ts
@@ -7,29 +7,25 @@ import {category, task} from "../interfaces";
 export class TransferringCategoryService {
 
   GetUserTask(tasks: task[]) {
-    let login = localStorage.getItem('авторизован');
-    if (login) {
-      let userData = JSON.parse(login);
-      userData.tasks = tasks;
-      localStorage.setItem(login, JSON.stringify(userData));
-    }
+    let login = localStorage.getItem('авторизован') ?? '';
+    login = localStorage.getItem(login) ?? '';
+    let userData = JSON.parse(login);
+    userData.tasks = tasks;
+    localStorage.setItem(login, JSON.stringify(userData))
   }
 
   getUserCategory(categories: category[]) {
-    let login = localStorage.getItem('авторизован');
-    if (login) {
-      let userData = JSON.parse(login);
-      userData.categories = categories;
-      localStorage.setItem(login, JSON.stringify(userData));
-    }
-
+    let login = localStorage.getItem('авторизован') ?? '';
+    login = localStorage.getItem(login) ?? '';
+    let userData = JSON.parse(login)
+    userData.categories = categories
+    localStorage.setItem(login, JSON.stringify(userData))
   }
 
   deleteTask(task: task) {
-    let login = localStorage.getItem('авторизован');
-    if (login){
-      let userData = JSON.parse(login);
-      userData.tasks;
-    }
+    let login = localStorage.getItem('авторизован') ?? ''
+    login = localStorage.getItem(login) ?? '';
+    let userData = JSON.parse(login)
+    userData.tasks
   }
 }
