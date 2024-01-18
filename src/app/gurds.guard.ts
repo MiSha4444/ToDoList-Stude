a2d367c764507ef0b330669129e11557c4da1ece
MSHabanov <misnash4895@gmail.com>
2024-01-18 15:59:46 +0300

    Добавленно изменение в гуарды

--- src/app/gurds.guard.ts
+++ src/app/gurds.guard.ts
@@ -1,5 +1,11 @@
 import {CanActivateFn} from '@angular/router';
 
-export const gurdsGuard: CanActivateFn = (route, state) => {
-    return  localStorage.getItem('авторизован') !== '';
+export const gurdsGuard: CanActivateFn = (route, state): boolean => {
+  return localStorage.getItem('авторизован') !== '';
+};
+
+
+
+export const GuardAuth: CanActivateFn = (route, state): boolean => {
+  return localStorage.getItem('авторизован') === '';
 };
