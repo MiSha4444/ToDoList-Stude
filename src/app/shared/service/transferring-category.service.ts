2d2a9710d072afc7970fcf72848842bdd6dcedd4
MSHabanov <misnash4895@gmail.com>
2024-01-18 15:56:27 +0300

    Изменен способо получения и имзенения Задач и категорий

--- src/app/shared/service/transferring-category.service.ts
+++ src/app/shared/service/transferring-category.service.ts
@@ -1,18 +1,28 @@
 import {Category, Task} from "../interfaces";
+import {BehaviorSubject, Observable} from "rxjs";
 
 export class TransferringCategoryService {
+  private tasksSubject: BehaviorSubject<Task[]> = new BehaviorSubject<Task[]>([]);
+  private categoriesSubject: BehaviorSubject<Category[]> = new BehaviorSubject<Category[]>([]);
 
-  GetUserTask(tasks: Task[]) {
-    let login = localStorage.getItem('авторизован') ?? '';
+  public tasks$: Observable<Task[]> = this.tasksSubject.asObservable();
+  public categories$: Observable<Category[]> = this.categoriesSubject.asObservable();
+
+  public updateUserTasks(tasks: Task[]) {
+    let login: string = localStorage.getItem('авторизован') ?? '';
     let userData = JSON.parse(localStorage.getItem(login) ?? '');
     userData.tasks = tasks;
     localStorage.setItem(login, JSON.stringify(userData));
+
+    this.tasksSubject.next(tasks);
   }
 
-  getUserCategory(categories: Category[]) {
-    let login = localStorage.getItem('авторизован') ?? '';
+  public updateUserCategories(categories: Category[]) {
+    let login: string = localStorage.getItem('авторизован') ?? '';
     let userData = JSON.parse(localStorage.getItem(login) ?? '');
     userData.categories = categories;
     localStorage.setItem(login, JSON.stringify(userData));
+
+    this.categoriesSubject.next(categories);
   }
 }
