33f69a87c7cea8688a76dfe2b040b2392d57e8e7
MSHabanov <misnash4895@gmail.com>
2024-01-30 17:06:36 +0300

    refactor

--- src/app/tasks/сomponents/tasks/tasks.component.ts
+++ src/app/tasks/сomponents/tasks/tasks.component.ts
@@ -70,7 +70,7 @@ export class TasksComponent implements OnInit {
     this.task = this.taskForm.value;
     this.$submitted.next(false);
     this.taskDialog = true;
-  };
+  }
 
   public saveTask() {
     this.$submitted.next(true);
@@ -112,7 +112,7 @@ export class TasksComponent implements OnInit {
     })
     this.task = {...task};
     this.taskDialog = true;
-  }
+  };
 
 
   private createId(): string {
